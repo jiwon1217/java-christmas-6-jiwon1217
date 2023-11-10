0845b9230160adc36931a0ed024a600cb145cc81
jiwon1217 <koagjw@gmail.com>
2023-11-10 15:58:23 +0900

    :sparkles: feat: 주문 메뉴-주문 개수 입력 형식 준수, 개수는 1 이상의 숫자만 가능

--- src/main/java/christmas/util/Validator.java
+++ src/main/java/christmas/util/Validator.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final Pattern BETWEEN_1_AND_31_REGEX = Pattern.compile("^(?:[1-9]|[12]\\d|3[01])$");
+    private static final Pattern ORDER_MENU_REGEX = Pattern.compile("^[가-힣]+-\\d+(,[가-힣]+-\\d+)*$");
 
     public static void validateVisitDay(String input) {
         if (!isBetween1And31(input)) {
@@ -13,5 +14,19 @@ public class Validator {
 
     public static boolean isBetween1And31(String input) {
         return BETWEEN_1_AND_31_REGEX.matcher(input).matches();
+    }
+
+    public static void validateOrderMenu(String input) {
+        validateInputFormat(input);
+    }
+
+    private static void validateInputFormat(String input) {
+        if (!isValidInputFormat(input)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isValidInputFormat(String input) {
+        return ORDER_MENU_REGEX.matcher(input).matches();
     }
 }
