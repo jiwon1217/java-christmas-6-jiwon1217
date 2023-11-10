54de9e28866aa5dbef6771991698f51aa7725a75
jiwon1217 <koagjw@gmail.com>
2023-11-10 19:39:44 +0900

    :recycle: refactor: 입력 시 에러가 발생하면 에러 메시지를 출력하고 다시 입력 받는다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,19 +6,31 @@ import christmas.util.Validator;
 
 public class InputView {
     public static VisitDate askVisitDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
+        try {
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            String input = Console.readLine();
 
-        Validator.validateVisitDay(input);
-        int visitDay = Integer.parseInt(input);
+            Validator.validateVisitDay(input);
+            int visitDay = Integer.parseInt(input);
 
-        return new VisitDate(visitDay);
+            return new VisitDate(visitDay);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askVisitDate();
+        }
     }
 
     public static String askOrderMenu() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input = Console.readLine();
-        Validator.validateOrderMenu(input);
-        return input;
+        try {
+            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+            String input = Console.readLine();
+
+            Validator.validateOrderMenu(input);
+
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askOrderMenu();
+        }
     }
 }
