3ca2d0ae93d8b8176cd5309c7f45f6c6930b4ed6
jiwon1217 <koagjw@gmail.com>
2023-11-10 15:39:31 +0900

    :sparkles: feat: 주문 메뉴와 개수 묻는 문구 출력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -14,4 +14,8 @@ public class InputView {
 
         return new VisitDate(visitDay);
     }
+
+    public static void askOrderMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
 }
