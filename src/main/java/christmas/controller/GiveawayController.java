ff61897ae3824d92c2381e46246b446f86c735e5
jiwon1217 <koagjw@gmail.com>
2023-11-11 20:31:33 +0900

    :sparkles: feat: 증정 메뉴 관련 기능 조립

--- src/main/java/christmas/controller/GiveawayController.java
+++ src/main/java/christmas/controller/GiveawayController.java
@@ -3,10 +3,27 @@ package christmas.controller;
 import christmas.model.GiveawayDetails;
 import christmas.model.Menu;
 import christmas.model.OrderAmount;
+import christmas.model.PaymentInformation;
+import christmas.view.OutputView;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class GiveawayController {
     private static final int GIVEAWAY_THRESHOLD = 120_000;
     private static final int GIVEAWAY_QUANTITY = 1;
+
+    public GiveawayDetails give(PaymentInformation paymentInformation) {
+        OrderAmount orderAmount = paymentInformation.orderAmount();
+
+        Map<Menu, Integer> giveawayInformation = new EnumMap<>(Menu.class);
+        GiveawayDetails giveawayDetails = new GiveawayDetails(giveawayInformation);
+
+        if (isPossibleGive(orderAmount)) {
+            getGiveaway(giveawayDetails);
+        }
+        OutputView.printGiveawayDetails(giveawayDetails);
+        return giveawayDetails;
+    }
 
     public boolean isPossibleGive(OrderAmount orderAmount) {
         return orderAmount.orderAmount() >= GIVEAWAY_THRESHOLD;
