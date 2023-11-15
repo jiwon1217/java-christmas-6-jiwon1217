fc5f6debb931bfc473082b88070fd9c0eafb7cf0
jiwon1217 <koagjw@gmail.com>
2023-11-15 16:20:45 +0900

    :sparkles: feat: 결제 금액 관련 기능 조립

---
+++ src/main/java/christmas/controller/PaymentController.java
@@ -0,0 +1,17 @@
+package christmas.controller;
+
+import christmas.model.BenefitInformation;
+import christmas.model.PayAmount;
+import christmas.model.PaymentInformation;
+import christmas.util.Calculator;
+import christmas.view.OutputView;
+
+public class PaymentController {
+    public PayAmount pay(PaymentInformation paymentInformation, BenefitInformation benefitInformation) {
+        PayAmount payAmount = Calculator.calculatePayAmount(paymentInformation, benefitInformation);
+
+        OutputView.printPayAmount(payAmount);
+
+        return payAmount;
+    }
+}
