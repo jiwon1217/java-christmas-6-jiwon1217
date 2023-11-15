3b949109a2997f44c95ff608985bfd7a39fb86c9
jiwon1217 <koagjw@gmail.com>
2023-11-15 23:38:50 +0900

    :recycle: refactor: 메서드 분리

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,8 +1,8 @@
 package christmas.controller;
 
 import christmas.model.order.OrderAmount;
-import christmas.model.order.OrderList;
 import christmas.model.order.OrderInformation;
+import christmas.model.order.OrderList;
 import christmas.model.order.VisitDate;
 import christmas.util.AmountCalculator;
 import christmas.util.Parser;
@@ -13,15 +13,27 @@ public class OrderController {
     public OrderInformation order() {
         OutputView.printGreeting();
 
-        VisitDate visitDate = new VisitDate(InputView.askVisitDate());
-        OrderList orderList = Parser.parseStringToOrderList(InputView.askOrderMenu());
+        VisitDate visitDate = getVisitDate();
+        OrderList orderList = getOrderList();
 
         OutputView.printBenefitPreviewInformation(visitDate);
         OutputView.printOrderList(orderList);
 
-        OrderAmount amount = AmountCalculator.calculateOrderAmount(orderList);
+        OrderAmount amount = calculateOrderAmount(orderList);
         OutputView.printOrderAmount(amount);
 
         return new OrderInformation(visitDate, orderList, amount);
+    }
+
+    private OrderList getOrderList() {
+        return Parser.parseStringToOrderList(InputView.askOrderMenu());
+    }
+
+    private VisitDate getVisitDate() {
+        return new VisitDate(InputView.askVisitDate());
+    }
+
+    private OrderAmount calculateOrderAmount(OrderList orderList) {
+        return AmountCalculator.calculateOrderAmount(orderList);
     }
 }
