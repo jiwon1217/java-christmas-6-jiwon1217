2683d8188975ed6b31d4ecec0c082958a2c9ad3c
jiwon1217 <koagjw@gmail.com>
2023-11-15 16:58:11 +0900

    :sparkles: feat: 크리스마스 프로모션 관련 기능 조립

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,29 @@
 package christmas;
 
+import christmas.controller.BadgeController;
+import christmas.controller.BenefitAmountController;
+import christmas.controller.BenefitController;
+import christmas.controller.GiveawayController;
+import christmas.controller.OrderController;
+import christmas.controller.PaymentController;
+import christmas.model.BenefitDetails;
+import christmas.model.BenefitInformation;
+import christmas.model.GiveawayDetails;
+import christmas.model.PayAmount;
+import christmas.model.PaymentInformation;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PaymentInformation paymentInformation = new OrderController().order();
+
+        GiveawayDetails giveawayDetails = new GiveawayController().give(paymentInformation);
+
+        BenefitDetails benefitDetails = new BenefitController().apply(paymentInformation, giveawayDetails);
+
+        BenefitInformation benefitInformation = new BenefitAmountController().getBenefitAmount(benefitDetails);
+
+        PayAmount payAmount = new PaymentController().pay(paymentInformation, benefitInformation);
+
+        new BadgeController().give(benefitInformation);
     }
 }
