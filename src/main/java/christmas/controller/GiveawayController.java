a04b070d51a8446f9b471f1b730a8eafefaeda37
jiwon1217 <koagjw@gmail.com>
2023-11-15 23:32:25 +0900

    :recycle: refactor: 메서드 분리

--- src/main/java/christmas/controller/GiveawayController.java
+++ src/main/java/christmas/controller/GiveawayController.java
@@ -10,14 +10,20 @@ import java.util.Map;
 public class GiveawayController {
     public GiveawayDetails give(OrderInformation orderInformation) {
         int amount = orderInformation.orderAmount();
+        GiveawayDetails giveawayDetails = calculateGiveawayDetails(amount);
 
+        OutputView.printGiveawayDetails(giveawayDetails);
+
+        return giveawayDetails;
+    }
+
+    private GiveawayDetails calculateGiveawayDetails(int amount) {
         Map<Menu, Integer> giveawayInformation = new EnumMap<>(Menu.class);
         GiveawayDetails giveawayDetails = new GiveawayDetails(giveawayInformation);
 
         if (giveawayDetails.isPossibleGetGiveaway(amount)) {
             giveawayDetails.getGiveaway(giveawayDetails);
         }
-        OutputView.printGiveawayDetails(giveawayDetails);
 
         return giveawayDetails;
     }
